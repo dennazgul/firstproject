804654f62d6788dc899b79061113fc16b60f9f9d
dennazgul <nazguldragon@mail.ru>
2016-05-18 23:48:23 +0300

    попытка сделать реакцию на ввод букв вместо чисел

--- second task/script/calculator.js
+++ second task/script/calculator.js
@@ -1,16 +1,16 @@
 /*function ts() {
     var myel = document.getElementById('itog')
-    var ks = document.getElementById('znak').value;
-    var b1 = document.getElementById('b1').value
-    var b2 = document.getElementById('b2').value
+    var ks = document.getElementById('znak').value
+    var b1 = Number(document.getElementById('b1').value)
+    var b2 = Number(document.getElementById('b2').value)
     if (ks == 1) {
-        ks = b1 * b2;
+        ks = b1 * b2
     } else if (ks == 2) {
-        ks = b1 / b2;
+        ks = b1 / b2
     } else if (ks == 3) {
         ks = b1 + b2
     } else {
-        ks = b1 - b2;
+        ks = b1 - b2
     }
     myel.innerText = ks;
 }*/
@@ -21,11 +21,19 @@ function formatNumber(myNumber) {
     return myNumber;
 }
 
+function format(mygNumber) {
+    if (mygNumber == 'NaN') {
+        alert('введено недопустимое значение');
+    }
+    return mygNumber;
+}
+
+
 function ts() {
     var myel = document.getElementById('itog')
     var ks = document.getElementById('znak').value;
-    var b1 = document.getElementById('b1').value
-    var b2 = document.getElementById('b2').value
+    var b1 = Number(document.getElementById('b1').value)
+    var b2 = Number(document.getElementById('b2').value)
     if (ks == 1) {
         ks = b1 * b2;
     } else if (ks == 2) {
@@ -35,5 +43,5 @@ function ts() {
     } else {
         ks = b1 - b2;
     }
-    myel.innerText = ks;
+    myel.innerText = format(ks);
 }
