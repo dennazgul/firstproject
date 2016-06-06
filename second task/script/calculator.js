4398064cabe36d5424b889a9845405520ff122c0
dennazgul <nazguldragon@mail.ru>
2016-06-06 22:32:46 +0300

    Final version (изъянов почти нет. костылей тоже немного)

--- second task/script/calculator.js
+++ second task/script/calculator.js
@@ -27,67 +27,41 @@ function ts() {
     myel.innerText = ks1;
 }
 
-/*function PND1() {
-    var bnwv = document.getElementById('bnw1');
-    var b1 = document.getElementById('b1').value;
-    if (b1.length == 0) {
-        bnwv.innerText = ' пустое поле, введите цифры'
-        myel.innerText = '';
-    } else {
-        bnwv.innerText = ''
-    }
-
-
-}
-
-function PND2() {
-    var bnwv2 = document.getElementById('bnw2');
-    var b2 = document.getElementById('b2').value;
-    if (b2.length == 0) {
-        bnwv2.innerText = ' пустое поле, введитее цифры';
-    } else {
-        bnwv2.innerText = '';
-    }
-
-
-}
-
-function PNB() {
-    var bnwv2 = document.getElementById('bnw2');
-    var b2 = Number(document.getElementById('b2').value);
-    if (isNaN(b2)) {
-        bnwv2.innerText = ' вводите буквы';
-    } else {
-        bnwv2.innerText = '';
-    }
-
-
-}*/
 
 function ULTI2() {
     var bnwv2 = document.getElementById('bnw2');
     var myel = document.getElementById('itog');
     var ks = document.getElementById('znak').value;
+    var b2 = document.getElementById('b2')
     if (ks == 2) {
         if (PNB() < 229) {
             bnwv2.innerText = ' вводите буквы';
+            b2.style.borderColor = 'red';
         } else if (PND2() < 229) {
             bnwv2.innerText = ' пустая ячейка';
+            b2.style.borderColor = 'red';
         } else if (PNN() < 229) {
-            bnwv2.innerText = 'делить на ноль нельзя';
+            bnwv2.innerText = ' делить на ноль нельзя';
+            b2.style.borderColor = 'red';
             myel.innerText = ''
         } else {
-            bnwv2.innerText = ''
+            bnwv2.innerText = '';
+            b2.style.borderColor = 'black';
+            b2.style.borderWidth = '1px';
         }
 
     } else {
         if (PNB() < 229) {
             bnwv2.innerText = ' вводите буквы';
+            b2.style.borderColor = 'red';
         } else if (PND2() < 229) {
             bnwv2.innerText = ' пустая ячейка';
+            b2.style.borderColor = 'red';
         } else {
 
             bnwv2.innerText = '';
+            b2.style.borderColor = 'black';
+            b2.style.borderWidth = '1px';
         }
     }
 }
@@ -132,12 +106,18 @@ function PNN() {
 
 function ULTI1() {
     var bnwv1 = document.getElementById('bnw1');
+    var b1 = document.getElementById('b1')
     if (PNB1() < 229) {
         bnwv1.innerText = ' вводите буквы';
+        b1.style.borderColor = 'red';
     } else if (PND1() < 229) {
         bnwv1.innerText = ' пустая ячейка';
+        b1.style.borderColor = 'red';
+
     } else {
         bnwv1.innerText = ''
+        b1.style.borderColor = 'black';
+        b1.style.borderWidth = '1px';
     }
 }
 
